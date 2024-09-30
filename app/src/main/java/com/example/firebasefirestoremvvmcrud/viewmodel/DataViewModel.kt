3c3ca1ebcb6599bd963aba615b9ffd6e30f0c2b8
HandsomeRobot <alaminjoy456@gmail.com>
2024-09-30 13:30:30 +0600

    have to work data view model 1.06

--- app/src/main/java/com/example/firebasefirestoremvvmcrud/viewmodel/DataViewModel.kt
+++ app/src/main/java/com/example/firebasefirestoremvvmcrud/viewmodel/DataViewModel.kt
@@ -1,7 +1,11 @@
 package com.example.firebasefirestoremvvmcrud.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.firebasefirestoremvvmcrud.model.Data
+import com.example.firebasefirestoremvvmcrud.repository.DataRepository
 
 class DataViewModel: ViewModel() {
-
+    private val repository = DataRepository()
+    private val dataList:MutableLiveData<List<Data>> = DataRepository.fetchdata()
 }
