403790b5d4db1b9b82dc43b19c6aceaa58cce087
alamin <alaminjoy456@gmail.com>
2024-09-30 22:52:37 +0600

    data view model done

--- app/src/main/java/com/example/firebasefirestoremvvmcrud/MainActivity.kt
+++ app/src/main/java/com/example/firebasefirestoremvvmcrud/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.firebasefirestoremvvmcrud
 
 import android.os.Bundle
+import android.provider.ContactsContract.Data
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.MutableLiveData
+import com.example.firebasefirestoremvvmcrud.repository.DataRepository
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/example/firebasefirestoremvvmcrud/viewmodel/DataViewModel.kt
+++ app/src/main/java/com/example/firebasefirestoremvvmcrud/viewmodel/DataViewModel.kt
@@ -1,11 +1,30 @@
 package com.example.firebasefirestoremvvmcrud.viewmodel
 
+import android.provider.ContactsContract
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.firebasefirestoremvvmcrud.model.Data
 import com.example.firebasefirestoremvvmcrud.repository.DataRepository
 
 class DataViewModel: ViewModel() {
-    private val repository = DataRepository()
-    private val dataList:MutableLiveData<List<Data>> = DataRepository.fetchdata()
+    private val dataRepository = DataRepository()
+    private var _datalist: MutableLiveData<List<Data>> = dataRepository.fetchData()
+    val dataList :LiveData<List<Data>> get() = _datalist
+
+    fun addData(data: Data,onSuccess:()->Unit,onFalure:() ->Unit){
+         dataRepository.addData(data)
+             .addOnSuccessListener { onSuccess() }
+             .addOnFailureListener{onFalure()}
+    }
+    fun updateData(data: Data){
+        dataRepository.updateData(data)
+    }
+    fun deleteData(data: Data,onSuccess:()->Unit,onFalure:() ->Unit){
+        dataRepository.deleteData(data)
+            .addOnSuccessListener { onSuccess() }
+            .addOnFailureListener{onFalure() }
+    }
+
+
 }
